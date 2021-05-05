20b119aefbf1ec8fb60abe4f8f83c4de8528f6de
Sheila <codergirlstl@users.noreply.github.com>
2021-05-04 21:27:46 -0500

    Update StringFunction.py
    Try tomorrow

--- StringFunction.py
+++ StringFunction.py
@@ -1,14 +1,1 @@
-def substring_check(myseason, yourseason):
-    season1 = str(myseason)
-    season2 = str(yourseason)
-    if season1 in season2:
-        answer = "True"
-    if season1 not in season2:
-        answer = "False"
-    return answer
 
-myseason = ("My favorite season is Summer.")
-
-yourseason = input("What is your favorite season")
-
-substring_check(myseason, yourseason)
