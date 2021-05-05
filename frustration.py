1be78ee1ac3c0890c256d2f5f4e8e61346fa4cc1
Sheila <codergirlstl@users.noreply.github.com>
2021-05-04 22:11:42 -0500

    Create frustration.py
    first try

---
+++ frustration.py
@@ -0,0 +1,7 @@
+
+#write and call a python function that takes input and returns it
+def feelings():
+    answer = input(“How did class go tonight?“)
+    return answer
+response = feelings()
+print(response)
